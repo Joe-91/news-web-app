008196f15e57c1e830d97f7481a814dd286ae465
Joe-91 <hafoury@yahoo.com>
2020-11-17 15:20:50 -0800

    Setup Vuetify

--- news-web-app/client/main.js
+++ news-web-app/client/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 
 import '../imports/ui/plugins'
 
-import App from '../imports/ui/App.vue'
+import App from '../imports/ui/App.vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Meteor.startup(() => {
+  
+  Vue.use(Vuetify); 
+
+  const vuetify = new Vuetify();
+
   new Vue({
-    el: '#app',
-    ...App,
-  })
+    render: (h) => h(App),
+    vuetify
+  }).$mount('#app')
+  
 })
--- news-web-app/imports/ui/App.vue
+++ news-web-app/imports/ui/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-    <h1>Welcome to Meteor 222</h1>
-    <hello/>
-    <info/>
+   <v-app id="vuetify-app">
+    <div>
+    <v-breadcrumbs
+      :items="items"
+      divider="-"
+    ></v-breadcrumbs>
   </div>
+  </v-app>
 </template>
 
 <script>
@@ -15,6 +18,25 @@ export default {
     Hello,
     Info,
   },
+  data: () => ({
+      items: [
+        {
+          text: 'Dashboard',
+          disabled: false,
+          href: 'breadcrumbs_dashboard',
+        },
+        {
+          text: 'Link 1',
+          disabled: false,
+          href: 'breadcrumbs_link_1',
+        },
+        {
+          text: 'Link 2',
+          disabled: true,
+          href: 'breadcrumbs_link_2',
+        },
+      ],
+    }),
 }
 </script>
 
